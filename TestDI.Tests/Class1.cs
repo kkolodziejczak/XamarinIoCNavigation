7b6be27093ef2ba8393f45dd675a8a4a1456b2fe
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2018-09-15 10:46:22 +0200

    Test dontnet core test project....

---
+++ TestDI.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using TestDI.Services;
+
+namespace TestDI.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            var service = new NavigationService(null, null);
+            Assert.Pass("Test");
+        }
+    }
+}
