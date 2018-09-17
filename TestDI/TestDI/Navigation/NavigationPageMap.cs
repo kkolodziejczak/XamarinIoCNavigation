754fbc39e1e2f8ea3e42190f77b58516f6b30916
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2018-09-17 08:53:37 +0200

    Remove enum dependecny from NavigationService
    To ensure that it can be used in any case not only with registered
    ApplicationPage Enum.

---
+++ TestDI/TestDI/Navigation/NavigationPageMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using TestDI.Pages;
+
+namespace TestDI.Navigation
+{
+    public static class NavigationPageMap
+    {
+        public static readonly Dictionary<string, Type> PageMap = new Dictionary<string, Type>
+        {
+            { ApplicationPage.MainMenuPage.ToString(), typeof(MainPage) },
+            { ApplicationPage.SideBar.ToString(), typeof(StartPage) },
+            { ApplicationPage.LoginPage.ToString(), typeof(LoginPage) },
+            { ApplicationPage.ListViewPage.ToString(), typeof(ListViewPage) },
+        };
+    }
+}
