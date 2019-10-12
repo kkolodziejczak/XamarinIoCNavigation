d5f964966e04e4d23c9dbdc4c30bb415c7b19d1a
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2019-10-12 17:56:09 +0200

    Add Dispose Strategy

---
+++ TestDI/TestDI/Common/DisposePageStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.BetterNavigation.Forms;
+using Xamarin.Forms;
+
+namespace TestDI.Common
+{
+    public class DisposePageStrategy : IPopStrategy
+    {
+        public async Task BeforePopAsync(Page pageToPop)
+        {
+            if (pageToPop is IDisposable disposablePage)
+            {
+                disposablePage.Dispose();
+            }
+        }
+    }
+}
